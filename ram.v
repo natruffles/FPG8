8025784036730f44524d3de21cb7b084c7e2999f
natruffles <rolfesnz@mail.uc.edu>
2023-07-01 10:58:19 -0400

    Closes #30

--- ram.v
+++ ram.v
@@ -3,49 +3,39 @@
 
 // Updated version to make sure that block ram is inferred by yosys
 module ram #(
-    parameter MEM_WIDTH = 16,
-    parameter MEM_DEPTH = 256,
     parameter INIT_FILE = ""
 ) (
     input clk,
     input w_en,
     input r_en,
-    input [ADDR_WIDTH - 1:0] w_addr,
-    input [ADDR_WIDTH - 1:0] r_addr,
-    input [MEM_WIDTH - 1:0] w_data,
-    output reg [MEM_WIDTH - 1:0] r_data
+    input [11:0] addr,
+    input [15:0] w_data,
+    output reg [15:0] r_data
 );
 
 // initialization template
 /*
-ram_new #(   
-    .MEM_WIDTH(), 
-    .MEM_DEPTH(), 
+ram #(   
     .INIT_FILE()
-) ram_new_inst0 (
+) ram_inst0 (
     .clk(),
     .w_en(),
     .r_en(),
-    .w_addr(),
-    .r_addr(),
+    .addr(),
     .w_data(),
     .r_data(),
-    .MDR_RAM_connect()
 );
 */
 
-// number of bits for address is logbase2(256) = 8 by default
-localparam ADDR_WIDTH = $clog2(MEM_DEPTH);
-
 // Declare memory
-reg [MEM_WIDTH - 1:0] mem [0:MEM_DEPTH - 1];
+reg [15:0] mem [0:4095];
 
 // Interact with the memory block
 always @(posedge clk) begin
     if (w_en) begin
-        mem[w_addr] <= w_data;
+        mem[{{4{addr[11]}}, addr[11:0]}] <= w_data;
     end else if (r_en) begin
-        r_data <= mem[r_addr];
+        r_data <= mem[{{4{addr[11]}}, addr[11:0]}];
     end
 end
 
