0082f9612c8b111ac4c4f7322167cfd87c2bed95
natruffles <rolfesnz@mail.uc.edu>
2023-07-01 11:07:22 -0400

    Closes #32

--- PSW.v
+++ PSW.v
@@ -2,12 +2,11 @@ module PSW (
     input clk,
     input reset,
     inout [15:0] DATA,  
-    output [15:0] REG_OUT_PSW, 
+    output [2:0] REG_OUT_PSW, 
     input latch, 
     input enable, 
     input [3:0] IR_opcode,
     input IR_S,
-    input Z_in,
     input [2:0] ALU_control,
     input CC_Z_in,
     input CC_N_in
@@ -24,7 +23,6 @@ PSW PSW_inst0 (
     .enable(), 
     .IR_opcode(),
     .IR_S(),
-    .Z_in(),
     .ALU_control(),
     .CC_Z_in(),
     .CC_N_in()
@@ -37,14 +35,14 @@ reg [15:0] r;
 // reset to 0 if reset is high 
 // input from bus if latch is high
 // input 2 bits (don't touch the other 14) from comparator if opcode
-//   represents an ALU operation, IR.S is true, Z_in (control signal) is true,
-//   and the ALU operation is one performed that is supported by the ALU operation opcode
+//   represents an ALU operation, IR.S is true, and the ALU operation is 
+//   one performed that is supported by the ALU operation opcode
 always @(posedge clk) begin
     if (reset) begin
         r <= 0;
     end else if (latch) begin
         r <= DATA;
-    end else if (IR_opcode >= 0 && IR_opcode <= 5 && IR_S && Z_in && ALU_control != 3'b111 & ALU_control != 3'b010) begin
+    end else if (IR_opcode >= 0 && IR_opcode <= 5 && IR_S && ALU_control != 3'b111 & ALU_control != 3'b010) begin
         r[0] <= CC_Z_in;
         r[1] <= CC_N_in;
     end
@@ -52,6 +50,6 @@ end
 
 // if enable, r is driven to data port, else no connection (high impedance)
 assign DATA = (enable)? r : 16'bZZZZZZZZZZZZZZZZ;
-assign REG_OUT_PSW = r;
+assign REG_OUT_PSW = r[2:0];
 
 endmodule
